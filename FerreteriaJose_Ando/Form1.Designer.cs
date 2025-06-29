e84a0f5e02e25babf01faeacc335e643bf94795d
Matias <Matias@LACAGEE>
2025-06-28 22:32:36 -0400

    necesito algo

--- FerreteriaJose_Ando/Form1.Designer.cs
+++ FerreteriaJose_Ando/Form1.Designer.cs
@@ -34,7 +34,7 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Producto = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -50,10 +50,11 @@
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.button3);
             this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(36, 48);
+            this.panel1.Controls.Add(this.btn_Producto);
+            this.panel1.Location = new System.Drawing.Point(27, 39);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(396, 537);
+            this.panel1.Size = new System.Drawing.Size(297, 436);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
@@ -61,9 +62,10 @@
             // 
             this.button6.BackColor = System.Drawing.Color.IndianRed;
             this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(44, 400);
+            this.button6.Location = new System.Drawing.Point(33, 325);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(291, 54);
+            this.button6.Size = new System.Drawing.Size(218, 44);
             this.button6.TabIndex = 5;
             this.button6.Text = "SALIR";
             this.button6.UseVisualStyleBackColor = false;
@@ -72,9 +74,10 @@
             // 
             this.button5.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(44, 326);
+            this.button5.Location = new System.Drawing.Point(33, 265);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(291, 54);
+            this.button5.Size = new System.Drawing.Size(218, 44);
             this.button5.TabIndex = 4;
             this.button5.Text = "REPORTES";
             this.button5.UseVisualStyleBackColor = false;
@@ -83,9 +86,10 @@
             // 
             this.button4.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(44, 256);
+            this.button4.Location = new System.Drawing.Point(33, 208);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(291, 54);
+            this.button4.Size = new System.Drawing.Size(218, 44);
             this.button4.TabIndex = 3;
             this.button4.Text = "VENTAS";
             this.button4.UseVisualStyleBackColor = false;
@@ -95,9 +99,10 @@
             // 
             this.button3.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(44, 180);
+            this.button3.Location = new System.Drawing.Point(33, 146);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(291, 54);
+            this.button3.Size = new System.Drawing.Size(218, 44);
             this.button3.TabIndex = 2;
             this.button3.Text = "CLIENTES";
             this.button3.UseVisualStyleBackColor = false;
@@ -106,33 +111,36 @@
             // 
             this.button2.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(44, 105);
+            this.button2.Location = new System.Drawing.Point(33, 85);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(291, 54);
+            this.button2.Size = new System.Drawing.Size(218, 44);
             this.button2.TabIndex = 1;
             this.button2.Text = "PROVEEDORES";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button1
+            // btn_Producto
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(44, 35);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(291, 54);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "PRODUCTOS";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_Producto.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_Producto.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Producto.Location = new System.Drawing.Point(33, 28);
+            this.btn_Producto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_Producto.Name = "btn_Producto";
+            this.btn_Producto.Size = new System.Drawing.Size(218, 44);
+            this.btn_Producto.TabIndex = 0;
+            this.btn_Producto.Text = "PRODUCTOS";
+            this.btn_Producto.UseVisualStyleBackColor = false;
+            this.btn_Producto.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Castellar", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(30, 9);
+            this.label1.Location = new System.Drawing.Point(22, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(824, 34);
+            this.label1.Size = new System.Drawing.Size(662, 26);
             this.label1.TabIndex = 0;
             this.label1.Text = "Sistema de Inventario Ferretería Jose_ando";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -141,9 +149,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Castellar", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(552, 65);
+            this.label2.Location = new System.Drawing.Point(414, 53);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(215, 34);
+            this.label2.Size = new System.Drawing.Size(172, 26);
             this.label2.TabIndex = 1;
             this.label2.Text = "Productos";
             // 
@@ -151,9 +160,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Castellar", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(1059, 65);
+            this.label3.Location = new System.Drawing.Point(794, 53);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(285, 34);
+            this.label3.Size = new System.Drawing.Size(231, 26);
             this.label3.TabIndex = 2;
             this.label3.Text = "VENTAS DEL MES";
             // 
@@ -161,24 +171,26 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Castellar", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(800, 374);
+            this.label4.Location = new System.Drawing.Point(600, 304);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(285, 34);
+            this.label4.Size = new System.Drawing.Size(231, 26);
             this.label4.TabIndex = 3;
             this.label4.Text = "VENTAS DEL MES";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(1494, 611);
+            this.ClientSize = new System.Drawing.Size(1120, 496);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -192,7 +204,7 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Producto;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button4;
